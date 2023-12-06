eed86998835108a4806463bdb2f07c3f031c1d07
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-12-06 17:40:05 +0200

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
 import Catalog from "../../domains/catalog/catalog";
 import CarouselComponent from "@/components/carousel";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const pathName = usePathname();
   return (
     <main className={`max-w-screen min-h-screen`}>
-      <CarouselComponent />
-      <Catalog />
+      <AnimatePresence>
+        <motion.div
+          key={pathName}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          transition={{ duration: 0.75 }}
+          variants={{
+            initialState: {
+              opacity: 0,
+            },
+            animateState: {
+              opacity: 1,
+            },
+            exitState: {},
+          }}
+        >
+          <CarouselComponent />
+          <Catalog />
+        </motion.div>
+      </AnimatePresence>
     </main>
   );
 }
