c6cc0ff33492d09c47da05c611a5b9ebcee29f79
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-12-10 16:19:45 +0200

    fixes

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -3,14 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
+    // gets searchparams
     const { searchParams } = new URL(req.url);
     const email = searchParams.get("email") as string;
+
+    // gets a user
     const user = await prisma.user.findFirst({ where: { email } });
+
+    // gets a cart based on user id
     const cart = await prisma.cart.findFirst({
       where: { userId: user?.id },
       include: { cartProducts: { include: { product: true } } },
     });
 
+    // in case if a cart doesn't exist, it creates a new one
     if (!cart) {
       const new_cart = await prisma.cart.create({
         data: {
@@ -33,29 +39,30 @@ export async function POST(req: NextRequest) {
     const user: any = await prisma.user.findUnique({
       where: { email: body.session.user.email },
     });
-    const cart = await prisma.cart.findFirst({ where: { userId: user?.id } });
+    const cart = await prisma.cart.findFirst({
+      where: { userId: user?.id },
+      include: { cartProducts: { include: { product: true } } },
+    });
 
-    if (!cart) {
-      const new_cart = await prisma.cart.create({
-        data: {
-          userId: user.id,
-          cartProducts: { create: { productId: body.data.id } },
-        },
-        include: { cartProducts: true },
-      });
-      return new NextResponse(JSON.stringify(new_cart), {
+    const existingCartProduct: any = cart?.cartProducts.filter(
+      (cartProduct) => cartProduct.productId === body.data.id
+    );
+
+    if (existingCartProduct[0]) {
+      return new NextResponse(JSON.stringify(cart), {
         status: 200,
       });
     }
 
     await prisma.cartProduct.create({
       data: {
-        cartId: cart.id,
+        cartId: cart?.id!,
         productId: body.data.id,
       },
     });
+
     const updatedCart = await prisma.cart.findUnique({
-      where: { id: cart.id },
+      where: { id: cart?.id },
       include: { cartProducts: { include: { product: true } } },
     });
     return new NextResponse(JSON.stringify(updatedCart), {
