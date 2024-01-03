4135d284491cc3555f04cad2c171c62b1f802f48
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-03 21:02:04 +0200

    create route for discount products

---
+++ src/app/api/discountProducts/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../auth/[...nextauth]/auth";
+
+export async function GET(req: NextRequest) {
+  try {
+    const products = await prisma.product.findMany({
+      where: { discount: { not: null } },
+      take: 10,
+    });
+
+    return new NextResponse(JSON.stringify(products), {
+      status: 200,
+    });
+  } catch (err) {
+    return new NextResponse(JSON.stringify(err), {
+      status: 500,
+    });
+  }
+}
