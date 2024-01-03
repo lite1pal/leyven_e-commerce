6136f4e3f2bed7062811a8506e2c2d83cbf7c34d
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-03 20:58:11 +0200

    add telegram api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 export const NOVAPOSHTA_API_URL = process.env.NEXT_PUBLIC_NOVAPOSHTA_API_URL!;
 export const NOVAPOSHTA_API_KEY = process.env.NEXT_PUBLIC_NOVAPOSHTA_API_KEY!;
+export const TELEGRAM_API_KEY = process.env.NEXT_PUBLIC_TELEGRAM_API_KEY!;
+export const TELEGRAM_API_URL = process.env.NEXT_PUBLIC_TELEGRAM_API_URL!;
+export const TELEGRAM_CHAT_ID = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID!;
