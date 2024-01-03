ee1f91cf0f821be057fc75db96ff907cafcc7ede
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-03 20:56:52 +0200

    add route with catalogs of different categories

---
+++ src/app/category/[category]/page.tsx
@@ -0,0 +1,17 @@
+import CategoryHeader from "@/components/categoryHeader";
+import { API_URL } from "@/config/api";
+import CatalogView from "@/components/catalog";
+import { categories } from "@/data/categories";
+import BasicBreadcrumbs from "@/components/breadCrumbs";
+import Categories from "@/components/categories";
+import CategoryView from "@/domains/category/category";
+import { Suspense } from "react";
+import MySpinner from "@/components/base/Spinner";
+
+export default async function Category({ params, searchParams }: any) {
+  return (
+    <Suspense fallback={<MySpinner />}>
+      <CategoryView {...{ params, searchParams }} />
+    </Suspense>
+  );
+}
