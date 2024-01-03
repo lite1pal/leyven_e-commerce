8d5019e84d393b48f4ca6557bd9028fb869a3d40
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-03 21:01:32 +0200

    add nextjs not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Button from "@/components/base/Button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="mx-auto mt-36 flex w-fit flex-col gap-10 text-center">
+      <h2 className="text-3xl font-medium">404</h2>
+      <h2 className="text-3xl font-medium">Сторінку не знайдено</h2>
+
+      <Link href="/">
+        <Button title="Повернутися до головної сторінкі" />
+      </Link>
+    </div>
+  );
+}
