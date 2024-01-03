753ba13c219718af255872cba12092af2f8c2d27
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-03 21:01:07 +0200

    create categories object

---
+++ src/data/categories.ts
@@ -0,0 +1,104 @@
+export const categories: any = {
+  "goduvannya-domashnih-tvarin": {
+    name: "Годування домашніх тварин і птахів",
+    route: "/category/goduvannya-domashnih-tvarin",
+    subCategories: {
+      kormi: {
+        name: "Корми",
+        route: "/category/goduvannya-domashnih-tvarin/kormi",
+      },
+      pauchi: {
+        name: "Паучі",
+        route: "/category/goduvannya-domashnih-tvarin/pauchi",
+      },
+      lasoschi: {
+        name: "Ласощі",
+        route: "/category/goduvannya-domashnih-tvarin/lasoschi",
+      },
+      konservi: {
+        name: "Консерви",
+        route: "/category/goduvannya-domashnih-tvarin/konservi",
+      },
+    },
+  },
+  "tovari-dlya-progulyanok": {
+    name: "Товари для прогулянок і подорожей з тваринами",
+    route: "/category/tovari-dlya-progulyanok",
+    subCategories: {
+      "sumki-kontejneri-dlya": {
+        name: "Сумки і контейнери для перенесення домашніх тварин",
+        route: "/category/tovari-dlya-progulyanok/sumki-kontejneri-dlya",
+      },
+      "povidki-ruletki-nashijniki": {
+        name: "Повідки, нашийники, намордники, шлейки",
+        route: "/category/tovari-dlya-progulyanok/povidki-ruletki-nashijniki",
+      },
+    },
+  },
+  "tovari-dlya-komfortu": {
+    name: "Товари для комфорту домашніх тварин",
+    route: "/category/tovari-dlya-komfortu",
+    subCategories: {
+      "igrashki-dlya-domashnih": {
+        name: "Іграшки для домашніх тварин",
+        route: "/category/tovari-dlya-komfortu/igrashki-dlya-domashnih",
+      },
+    },
+  },
+  "tovari-dlya-doglyadu": {
+    name: "Товари для догляду за домашніми тваринами",
+    route: "/category/tovari-dlya-doglyadu",
+    subCategories: {
+      "zasobi-dlya-gigiyeni": {
+        name: "Засоби для гігієни тварин",
+        route: "/category/tovari-dlya-doglyadu/zasobi-dlya-gigiyeni",
+      },
+      "obladnannya-instrumenti-dlya": {
+        name: "Обладнання та інструменти для грумінгу",
+        route: "/category/tovari-dlya-doglyadu/obladnannya-instrumenti-dlya",
+      },
+      "tualetni-lotki-dlya": {
+        name: "Туалетні лотки для тварин і аксесуари",
+        route: "/category/tovari-dlya-doglyadu/tualetni-lotki-dlya",
+      },
+    },
+  },
+  "veterinarni-zasobi-preparati": {
+    name: "Ветеринарні засоби та препарати",
+    route: "/category/veterinarni-zasobi-preparati",
+    subCategories: {
+      "antiparazitarni-zasobi-dlya": {
+        name: "Антипаразитарні засоби для тварин",
+        route:
+          "/category/veterinarni-zasobi-preparati/antiparazitarni-zasobi-dlya",
+      },
+      "vitamini-dlya-tvarin": {
+        name: "Вітаміни для тварин",
+        route: "/category/veterinarni-zasobi-preparati/vitamini-dlya-tvarin",
+      },
+      "veterinarni-antibiotiki-antimikrobni": {
+        name: "Ветеринарні антибіотики та антимікробні препарати",
+        route:
+          "/category/veterinarni-zasobi-preparati/veterinarni-antibiotiki-antimikrobni",
+      },
+      "antiseptichni-veterinarni-preparati": {
+        name: "Антисептичні ветеринарні препарати",
+        route:
+          "/category/veterinarni-zasobi-preparati/antiseptichni-veterinarni-preparati",
+      },
+      "veterinarni-zasobi-dlya": {
+        name: "Ветеринарні засоби для очей і вух",
+        route: "/category/veterinarni-zasobi-preparati/veterinarni-zasobi-dlya",
+      },
+      "vaktsini-dlya-tvarin": {
+        name: "Вакцини для тварин",
+        route: "/category/veterinarni-zasobi-preparati/vaktsini-dlya-tvarin",
+      },
+    },
+  },
+  //   "veterinarni-instrumenti-inventar": {
+  //     name: "Ветеринарні інструменти та інвентар",
+  //     route:
+  //       "/category/veterinarni-zasobi-preparati/veterinarni-instrumenti-inventar",
+  //   },
+};
