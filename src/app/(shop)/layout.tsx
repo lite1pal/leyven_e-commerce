df1de34c77af8f5167e97733178a2fa549cba656
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-07 20:56:12 +0200

    put script tag above body tag

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -23,21 +23,20 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-        />
-        <Script strategy="lazyOnload">
-          {`
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                   
                     gtag('config', '${GOOGLE_ANALYTICS_ID}');
                 `}
-        </Script>
-
+      </Script>
+      <body className={inter.className}>
         <SpeedInsights />
         <ServerCartProvider>
           <Toaster />
