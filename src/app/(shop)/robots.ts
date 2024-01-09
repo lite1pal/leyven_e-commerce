27b757df96a256fab49258cebe16040b08d63943
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-09 13:46:17 +0200

    update meta

---
+++ src/app/(shop)/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: "https://www.leyven.com.ua/sitemap.xml",
+    host: "https://www.leyven.com.ua",
+  };
+}
--- src/app/(shop)/sitemap.ts
+++ src/app/(shop)/sitemap.ts
@@ -5,16 +5,27 @@ export default async function sitemap() {
   const allProducts = await res.json();
 
   const products = allProducts.map((product: any) => ({
-    url: `https://leyven.vercel.app/product/${product.id}`,
+    url: `https://www.leyven.com.ua/product/${product.id}`,
     lastModified: new Date().toISOString(),
   }));
 
-  const routes = ["", "/product", "/about", "/contacts", "/category"].map(
-    (route) => ({
-      url: `https://leyven.vercel.app${route}`,
-      lastModified: new Date().toISOString(),
-    }),
-  );
+  const routes = [
+    "",
+    "/product",
+    "/about",
+    "/contacts",
+    "allProducts",
+    "search",
+    "/category",
+    "/category/goduvannya-domashnih-tvarin",
+    "/category/tovari-dlya-progulyanok",
+    "/category/tovari-dlya-komfortu",
+    "/category/tovari-dlya-doglyadu",
+    "/category/veterinarni-zasobi-preparati",
+  ].map((route) => ({
+    url: `https://www.leyveb.com.ua${route}`,
+    lastModified: new Date().toISOString(),
+  }));
 
   return [...routes, ...products];
 }
