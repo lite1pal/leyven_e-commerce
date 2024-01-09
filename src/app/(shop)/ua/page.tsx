f339951a8e3a541883491a1aee2bcda3bf28d503
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-09 14:35:06 +0200

    replace redirect on router.push

--- src/app/(shop)/ua/page.tsx
+++ src/app/(shop)/ua/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
-  redirect("/");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  });
+  return <div></div>;
 }
