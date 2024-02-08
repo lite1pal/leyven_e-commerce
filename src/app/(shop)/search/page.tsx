641bc58d8a24c9ba4b1b0081903718b6534af1bf
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-02-08 16:25:02 +0200

    update header

--- src/app/(shop)/search/page.tsx
+++ src/app/(shop)/search/page.tsx
@@ -20,7 +20,11 @@ export default async function SearchScreen({ searchParams, params }: any) {
 
   return (
     <div className="flex flex-grow flex-col">
-      <CategoryHeader title="Результат пошуку" />
+      <div
+        className={`px-4 py-5 font-sans text-3xl font-medium text-slate-900`}
+      >
+        Результат пошуку
+      </div>
       <div className="ml-10 flex items-center gap-1 pb-5">
         <Link href="/">
           <div className="cursor-pointer rounded-lg border-2 border-blue-600 border-opacity-0 p-1.5 transition duration-300  hover:border-opacity-100 hover:text-blue-600">
