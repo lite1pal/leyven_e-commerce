03bd3b5fafb0808f72b44ced2b88ec82d3b71dbc
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-02-08 16:55:50 +0200

    format description

--- src/domains/shop/product/components/description.tsx
+++ src/domains/shop/product/components/description.tsx
@@ -4,19 +4,6 @@ import SectionHeader from "@/components/base/SectionHeader";
 import Warning from "./warning";
 import { type Product } from "@/types";
 import { useState } from "react";
-import { Textarea } from "flowbite-react";
-
-// function insertLineBreaks(text, maxLength) {
-//   let result = "";
-//   let i = 0;
-
-//   while (i < text.length) {
-//     result += text.slice(i, i + maxLength) + "\n";
-//     i += maxLength;
-//   }
-
-//   return result;
-// }
 
 export default function Description({ data }: { data: Product }) {
   const [showMore, setShowMore] = useState(false);
@@ -28,32 +15,34 @@ export default function Description({ data }: { data: Product }) {
       setShowMore(true);
     }
   };
+
+  const paragraphs = data.description.split("\n");
+
   return (
     <div className="flex w-full flex-col gap-5 px-7 py-5 lg:w-1/2">
       <SectionHeader>Опис</SectionHeader>
 
-      {/* <Textarea
-        defaultValue={data.description}
-        className="overflow-y-scroll border-none p-0 text-base text-slate-900 focus:pointer-events-none"
-        rows={5}
-      /> */}
-
-      {/* <p>{insertLineBreaks(data.description, 100)}</p> */}
-
-      <p className="prose font-medium text-slate-600">
-        {showMore
-          ? formatDescription(data.description)
-          : formatDescription(data.description.slice(0, 500))}
-        {/* {!showMore && data.description.length > 450 && "... "} */}
-        {data.breadcrumbs.includes("Ветеринарія") && showMore && <Warning />}
-      </p>
-      {data.description.length >= 500 && (
-        <div
-          className="cursor-pointer text-lg text-blue-600"
-          onClick={handleShowMore}
-        >
-          Читати {showMore ? "менше" : "повністю"}
-        </div>
+      {paragraphs.length > 1 ? (
+        paragraphs.map((paragraph, i) => <p key={i}>{paragraph}</p>)
+      ) : (
+        <>
+          <p className="prose font-medium text-slate-600">
+            {showMore
+              ? formatDescription(data.description)
+              : formatDescription(data.description.slice(0, 500))}
+            {data.breadcrumbs.includes("Ветеринарія") && showMore && (
+              <Warning />
+            )}
+          </p>
+          {data.description.length >= 500 && (
+            <div
+              className="cursor-pointer text-lg text-blue-600"
+              onClick={handleShowMore}
+            >
+              Читати {showMore ? "менше" : "повністю"}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
