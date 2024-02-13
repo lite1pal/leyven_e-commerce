28d9a3ee6e18c611bb14a6cc8d1652305e3d81ea
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-02-13 19:50:04 +0200

    remove Suspense from dashboard/products route

--- src/app/(dashboard)/dashboard/products/page.tsx
+++ src/app/(dashboard)/dashboard/products/page.tsx
@@ -1,11 +1,5 @@
-import MySpinner from "@/components/base/Spinner";
 import ProductsView from "@/domains/dashboard/products/products";
-import { Suspense } from "react";
 
 export default function ProductsScreen() {
-  return (
-    <Suspense fallback={<MySpinner />}>
-      <ProductsView />
-    </Suspense>
-  );
+  return <ProductsView />;
 }
