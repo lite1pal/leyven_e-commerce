03a4ce73cd76904f63441a623af16f7a88c62f71
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-02-23 16:24:55 +0200

    Center a message that appears after successful order

--- src/app/(shop)/order_success/[id]/page.tsx
+++ src/app/(shop)/order_success/[id]/page.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 export default function OrderSuccess({ params }: any) {
   const id = params.id;
   return (
-    <div className="w-full flex flex-col gap-5 items-center mx-auto py-44">
-      <div className="w-fit mx-auto text-2xl">
-        Замовлення <span className="font-sans underline">№ {id}</span> успішне!
+    <div className="mx-auto flex w-full flex-col items-center gap-5 py-44">
+      <div className="mx-auto flex w-fit flex-col gap-3 text-center text-2xl sm:flex-row">
+        Замовлення{" "}
+        <span className="font-sans text-blue-600 max-sm:text-lg">№ {id}</span>{" "}
+        успішне!
       </div>
-      <h1 className="text-lg w-96 text-center">
+      <h1 className="w-96 text-center text-lg max-sm:px-3">
         Очікуйте дзвінок нашого менеджера для уточнення замовлення
       </h1>
       <Link href="/">
