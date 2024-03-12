0b08f506dc31a0b6987a7617443115be5bde3b92
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-03-12 19:21:24 +0200

    Change file names to follow my new name conventions

---
+++ src/app/(dashboard)/dashboard/products/components/info.tsx
@@ -0,0 +1,84 @@
+import { type Product } from "@/types";
+import { Label, TextInput } from "flowbite-react";
+import CloseIcon from "@mui/icons-material/Close";
+import toast from "react-hot-toast";
+import { useEffect } from "react";
+
+export default function EditInfo({
+  info,
+  setInfo,
+}: {
+  info: any;
+  setInfo: any;
+}) {
+  const addInfoItem = (newValue: string) => {
+    if (!newValue.includes(" - ")) {
+      toast.error(
+        "Впевніться, що нова характеристика має наступну форму: Назва - значення",
+      );
+      return;
+    }
+    if (!newValue.split(" - ")[0]) {
+      toast.error("Ви не додали назву");
+    }
+    if (!newValue.split(" - ")[1]) {
+      toast.error("Ви не додали значення");
+    }
+    const updatedInfo = [
+      ...info,
+      {
+        "g:attribute_name": { _text: newValue.split(" - ")[0] },
+        "g:attribute_value": { _text: newValue.split(" - ")[1] },
+      },
+    ];
+    setInfo(updatedInfo);
+  };
+
+  const deleteInfoItem = (item: any) => {
+    const filteredInfo = info.filter(
+      (i: any) =>
+        i["g:attribute_name"]._text !== item["g:attribute_name"]._text,
+    );
+    setInfo(filteredInfo);
+  };
+
+  return (
+    <div>
+      <div className="mb-2 block">
+        <Label
+          className="text-slate-900"
+          htmlFor="img"
+          value="Характеристики"
+        />
+      </div>
+      <TextInput
+        id="info"
+        onKeyDown={(e: any) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            addInfoItem(e.target.value);
+            e.target.value = "";
+          }
+        }}
+        placeholder="Додайте нову характеристику через дефіс з пробілами. Натисніть Enter, щоб додати"
+      />
+      <div className="mt-5 flex flex-wrap gap-3">
+        {info.map((item: any, i: number) => {
+          return (
+            <div
+              key={i}
+              className="flex w-fit cursor-default flex-wrap items-center gap-2 rounded-lg bg-blue-600 px-3 py-2 text-xs text-white"
+            >
+              <div onClick={() => deleteInfoItem(item)}>
+                <CloseIcon fontSize="small" />
+              </div>
+              <div>{item["g:attribute_name"]._text}</div>
+              <div>{"-"}</div>
+              <div>{item["g:attribute_value"]._text}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(dashboard)/dashboard/products/components/keywords.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuShortcut,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function EditKeywords({
+  keywords,
+  field,
+}: {
+  keywords: string;
+  field: any;
+}) {
+  const addKeyword = (keyword: string) => {
+    if (!keywords) {
+      field.onChange(keyword);
+      return;
+    }
+    const filteredKeywords = keywords.split(", ").concat([keyword]).join(", ");
+    field.onChange(filteredKeywords);
+  };
+
+  const deleteKeyword = (keyword: string) => {
+    const filteredKeywords = keywords
+      .split(", ")
+      .filter((key: string) => key !== keyword)
+      .join(", ");
+    field.onChange(filteredKeywords);
+  };
+
+  return (
+    <div className="w-full">
+      <Input
+        id="keywords"
+        onKeyDown={(e: any) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            addKeyword(e.target.value);
+            e.target.value = "";
+          }
+        }}
+        placeholder="Перечисліть ключові слова через кому та пробел. Натисніть Enter, щоб додати"
+      />
+
+      <div className="mt-5 flex w-full flex-wrap gap-3">
+        {keywords.length > 0 &&
+          keywords.split(", ").map((keyword: string, i) => {
+            return (
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <div
+                    key={i}
+                    className={
+                      "flex h-fit w-fit items-center gap-2 rounded-lg bg-emerald-200 px-3 py-2 text-xs font-medium"
+                    }
+                  >
+                    {keyword}
+                  </div>
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem onClick={() => deleteKeyword(keyword)}>
+                    Видалити
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
