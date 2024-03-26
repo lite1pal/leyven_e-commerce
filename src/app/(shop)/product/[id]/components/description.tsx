878eaeb5c3e37750c9302aa494ecce0878bd43f9
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-03-26 14:54:59 +0200

    Fix description's fit width

--- src/app/(shop)/product/[id]/components/description.tsx
+++ src/app/(shop)/product/[id]/components/description.tsx
@@ -3,8 +3,6 @@
 import SectionHeader from "@/components/section-header";
 import { type Product } from "@/types";
 import { useState } from "react";
-import parse from "html-react-parser";
-import ReactQuill from "react-quill";
 
 export default function Description({ data }: { data: Product }) {
   const [showMore, setShowMore] = useState(false);
@@ -21,22 +19,13 @@ export default function Description({ data }: { data: Product }) {
 
   const paragraphs = data.description.split("\n");
 
-  const isHTML = (str: string) => {
-    // const doc = new DOMParser().parseFromString(str, "text/html");
-    // return Array.from(doc.body.childNodes).some((node) => node.nodeType === 1);
-    // Regular expression to match HTML tags
+  const isHTML = (str: string): boolean => {
     const htmlRegex = /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/;
-    // Check if the string matches the HTML pattern
     return htmlRegex.test(str);
   };
 
-  // const isHTML = (str: string) => {
-  //   const doc = new DOMParser().parseFromString(str, "text/html");
-  //   return Array.from(doc.body.childNodes).some((node) => node.nodeType === 1);
-  // };
-
   return (
-    <div className="prose flex w-full flex-col gap-5 rounded-lg bg-white px-7 py-5 text-sm text-slate-900 prose-headings:text-slate-900 prose-p:text-slate-900 prose-strong:text-slate-900 prose-em:text-slate-900 lg:w-1/2">
+    <div className="prose flex max-w-full flex-1 flex-col gap-5 rounded-lg bg-white px-7 py-5 text-sm text-slate-900 prose-headings:text-slate-900 prose-p:text-slate-900 prose-strong:text-slate-900 prose-em:text-slate-900">
       <SectionHeader>Опис</SectionHeader>
 
       {isHTML(data.description) ? (
